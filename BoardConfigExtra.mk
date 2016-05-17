b434eb96427cf581ddfeb47a1f80dd9eb09db353
luca020400 <luca.stefani.ge1@gmail.com>
2016-05-17 19:11:44 +0200

    extra: Add qc-skia,opt,art,art-compiler

--- BoardConfigExtra.mk
+++ BoardConfigExtra.mk
@@ -3,3 +3,6 @@ DEVICE_PACKAGE_OVERLAYS += vendor/extra/blur/overlay
 
 # Blur
 TARGET_HAVE_UI_BLUR := true
+
+# QC Perf
+TARGET_HAVE_QC_PERF := true
--- product.mk
+++ product.mk
@@ -1,3 +1,17 @@
 # Blur
 PRODUCT_PACKAGES += \
-    libuiblur
+    libuiblur \
+    Blur.h
+
+# QC ART
+PRODUCT_PACKAGES += \
+    libqc-art \
+    libqc-art-compiler
+
+# QC Perf
+PRODUCT_PACKAGES += \
+    libqc-opt
+
+# QC Skia
+PRODUCT_PACKAGES += \
+    libqc-skia
