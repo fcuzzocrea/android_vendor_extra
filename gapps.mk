a36513243bf6d4be5521160d874ab0f9956659a1
Rashed Abdel-Tawab <rashed@linux.com>
2018-01-06 08:43:22 -0800

    Override with pixel launcher

--- gapps.mk
+++ gapps.mk
@@ -1,5 +1,6 @@
 GAPPS_VARIANT := super
 GAPPS_FORCE_PACKAGE_OVERRIDES := true
+GAPPS_FORCE_PIXEL_LAUNCHER := true
 
 $(call inherit-product-if-exists, vendor/opengapps/build/opengapps-packages.mk)
 
