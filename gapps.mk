4905b0ca5612a6a75f1595f091a37f8a0f0838a7
Rashed Abdel-Tawab <rashed@linux.com>
2018-10-10 16:39:22 -0700

    Build stock opengapps package and exclude hacked google cam

--- gapps.mk
+++ gapps.mk
@@ -1,11 +1,11 @@
 LOCAL_PATH := $(call my-dir)
 
-GAPPS_VARIANT := super
+GAPPS_VARIANT := stock
 GAPPS_FORCE_PACKAGE_OVERRIDES := true
 GAPPS_FORCE_PIXEL_LAUNCHER := true
 
 $(call inherit-product-if-exists, vendor/opengapps/build/opengapps-packages.mk)
 
 #ifneq ($(filter msm8996 msm8998,$(TARGET_BOARD_PLATFORM)),)
-PRODUCT_PACKAGES += GoogleCam
+#PRODUCT_PACKAGES += GoogleCam
 #endif # msm8996 msm8998
