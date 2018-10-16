7e7f2bb0f47d3e41c049b54797f58b9c2be6eb63
Rashed Abdel-Tawab <rashed@linux.com>
2018-10-16 09:51:17 -0700

    Remove unused Google Camera lines

--- gapps.mk
+++ gapps.mk
@@ -5,7 +5,3 @@ GAPPS_FORCE_PACKAGE_OVERRIDES := true
 GAPPS_FORCE_PIXEL_LAUNCHER := true
 
 $(call inherit-product-if-exists, vendor/opengapps/build/opengapps-packages.mk)
-
-#ifneq ($(filter msm8996 msm8998,$(TARGET_BOARD_PLATFORM)),)
-#PRODUCT_PACKAGES += GoogleCam
-#endif # msm8996 msm8998
