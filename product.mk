70c448e7362507a00890017d11be4e6766fc8f08
Nolen Johnson <johnsonnolen@gmail.com>
2019-01-09 19:55:13 +0000

    extra: Swap to production updater

--- product.mk
+++ product.mk
@@ -11,7 +11,7 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.config.vc_call_vol_steps=7 \
     ro.config.media_vol_steps=25
 
-PRODUCT_PROPERTY_OVERRIDES += lineage.updater.uri="http://updater.ods.ninja/api/v1/{device}/{type}/{incr}"
+PRODUCT_PROPERTY_OVERRIDES += lineage.updater.uri="http://updater.oddsolutions.us/api/v1/{device}/{type}/{incr}"
 
 -include vendor/google_pixel/product.mk
 
