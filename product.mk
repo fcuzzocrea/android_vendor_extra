8939979645530d615586318464b3c8d1a45f53c0
Nolen Johnson <johnsonnolen@gmail.com>
2019-07-10 00:47:01 -0400

    extra: Update MotoCamera2
    * Also add a missed ModService dependencies.

--- product.mk
+++ product.mk
@@ -47,6 +47,67 @@ endif
 ifneq ($(filter %nash,$(TARGET_PRODUCT)),)
 PRODUCT_PACKAGES += \
     MotCamera2
+
+PRODUCT_COPY_FILES := \
+    vendor/extra/MotCamera2/lib/libDepthBokehEffect.so:system/lib/libDepthBokehEffect.so \
+    vendor/extra/MotCamera2/lib/libFNVfbEngineHAL.so:system/lib/libFNVfbEngineHAL.so \
+    vendor/extra/MotCamera2/lib/libMobileOcrEngine.so:system/lib/libMobileOcrEngine.so \
+    vendor/extra/MotCamera2/lib/libMotDoF.so:system/lib/libMotDoF.so \
+    vendor/extra/MotCamera2/lib/libSNPE.so:system/lib/libSNPE.so \
+    vendor/extra/MotCamera2/lib/libSNPExecutor.so:system/lib/libSNPExecutor.so \
+    vendor/extra/MotCamera2/lib/libarcso:system/lib/ft_agegender_estimation.so:system/lib/ft_agegender_estimation.so \
+    vendor/extra/MotCamera2/lib/libarcso:system/lib/ft_beautyshot.so:system/lib/ft_beautyshot.so \
+    vendor/extra/MotCamera2/lib/libarcso:system/lib/ft_dualcam_refocus.so:system/lib/ft_dualcam_refocus.so \
+    vendor/extra/MotCamera2/lib/libbacktrace.so:system/lib/libbacktrace.so \
+    vendor/extra/MotCamera2/lib/libbsDepthWrapper_01_00_000.so:system/lib/libbsDepthWrapper_01_00_000.so \
+    vendor/extra/MotCamera2/lib/libc++_shared.so:system/lib/libc++_shared.so \
+    vendor/extra/MotCamera2/lib/libclearsight.so:system/lib/libclearsight.so \
+    vendor/extra/MotCamera2/lib/libclearsight_wrapper_01_00_000.so:system/lib/libclearsight_wrapper_01_00_000.so \
+    vendor/extra/MotCamera2/lib/libddm_wrapper_01_00_000.so:system/lib/libddm_wrapper_01_00_000.so \
+    vendor/extra/MotCamera2/lib/libdualcameraddm.so:system/lib/libdualcameraddm.so \
+    vendor/extra/MotCamera2/lib/libframe_stack_wrapper_morpho_v4.so:system/lib/libframe_stack_wrapper_morpho_v4.so \
+    vendor/extra/MotCamera2/lib/libframe_stack_wrapper_morpho_v5.so:system/lib/libframe_stack_wrapper_morpho_v5.so \
+    vendor/extra/MotCamera2/lib/libgif.so:system/lib/libgif.so \
+    vendor/extra/MotCamera2/lib/libjpeg.so:system/lib/libjpeg.so \
+    vendor/extra/MotCamera2/lib/liblenovo_panorama.so:system/lib/liblenovo_panorama.so \
+    vendor/extra/MotCamera2/lib/libmcf-common.so:system/lib/libmcf-common.so \
+    vendor/extra/MotCamera2/lib/libmcf-foundation.so:system/lib/libmcf-foundation.so \
+    vendor/extra/MotCamera2/lib/libmcf-foundation2.so:system/lib/libmcf-foundation2.so \
+    vendor/extra/MotCamera2/lib/libmcf-jni.so:system/lib/libmcf-jni.so \
+    vendor/extra/MotCamera2/lib/libmcf-local.so:system/lib/libmcf-local.so \
+    vendor/extra/MotCamera2/lib/libmcf-qc-fd-offline.so:system/lib/libmcf-qc-fd-offline.so \
+    vendor/extra/MotCamera2/lib/libmcf.so:system/lib/libmcf.so \
+    vendor/extra/MotCamera2/lib/libmcfutils.so:system/lib/libmcfutils.so \
+    vendor/extra/MotCamera2/lib/libmorpho_cinema_graph.so:system/lib/libmorpho_cinema_graph.so \
+    vendor/extra/MotCamera2/lib/libmorpho_defocusface.so:system/lib/libmorpho_defocusface \
+    vendor/extra/MotCamera2/lib/libmorpho_easy_hdr.so:system/lib/libmorpho_easy_hdr.so \
+    vendor/extra/MotCamera2/lib/libmorpho_image_stab5.so:system/lib/libmorpho_image_stab5.so \
+    vendor/extra/MotCamera2/lib/libmorpho_image_stabilizer4.so:system/lib/libmorpho_image_stabilizer4.so \
+    vendor/extra/MotCamera2/lib/libmorpho_panorama_gp3.so:system/lib/libmorpho_panorama_gp3.so \
+    vendor/extra/MotCamera2/lib/libmorpho_sensor_fusion.so:system/lib/libmorpho_sensor_fusion.so
+    vendor/extra/MotCamera2/lib/libmorpho_super_resolution.so:system/lib/libmorpho_super_resolution.so \
+    vendor/extra/MotCamera2/lib/libmotioncore_01_00_000.so:system/lib/libmotioncore_01_00_000.so \
+    vendor/extra/MotCamera2/lib/libmpbase.so:system/lib/libmpbase.so \
+    vendor/extra/MotCamera2/lib/liboffline_fd_wrapper_01_00_000.so:system/lib/liboffline_fd_wrapper_01_00_000.so \
+    vendor/extra/MotCamera2/lib/libopenssh.so:system/lib/libopenssh.so \
+    vendor/extra/MotCamera2/lib/librefocus_image_wrapper_01_00_000.so:system/lib/librefocus_image_wrapper_01_00_000.so \
+    vendor/extra/MotCamera2/lib/libremosaic_tuning.so:system/lib/libremosaic_tuning.so \
+    vendor/extra/MotCamera2/lib/libremosaic_wrapper.so:system/lib/libremosaic_wrapper.so \
+    vendor/extra/MotCamera2/lib/libremosaiclib.so:system/lib/libremosaiclib.so \
+    vendor/extra/MotCamera2/lib/libsnpe-android.so:system/lib/libsnpe-android.so \
+    vendor/extra/MotCamera2/lib/libsnpe_adsp.so:system/lib/libsnpe_adsp.so \
+    vendor/extra/MotCamera2/lib/libsnpe_dsp_domains.so:system/lib/libsnpe_dsp_domains.so \
+    vendor/extra/MotCamera2/lib/libsnpe_dsp_domains_skel.so:system/lib/libsnpe_dsp_domains_skel.so \
+    vendor/extra/MotCamera2/lib/libsnpe_dsp_skel.so:system/lib/libsnpe_dsp_skel.so \
+    vendor/extra/MotCamera2/lib/libsnpe_dsp_v65_domains_skel.so:system/lib/libsnpe_dsp_v65_domains_skel.so \
+    vendor/extra/MotCamera2/lib/libsuper_resolution_wrapper_morpho_v2.so:system/lib/libsuper_resolution_wrapper_morpho_v2.so \
+    vendor/extra/MotCamera2/lib/libsymphony-cpu.so:system/lib/libsymphony-cpu.so \
+    vendor/extra/MotCamera2/lib/libsymphonypower.so:system/lib/libsymphonypower.so \
+    vendor/extra/MotCamera2/lib/libubifocus_wrapper_01_00_000.so:system/lib/libubifocus_wrapper_01_00_000.so \
+    vendor/extra/MotCamera2/lib/libzxing_01_00_000.so:system/lib/libzxing_01_00_000.so \
+    vendor/extra/MotCamera2/lib/libzxingwrapper_01_00_000.so:system/lib/libzxingwrapper_01_00_000.so \
+    vendor/extra/MotCamera2/lib64/librcfw.so:system/lib64/librcfw.so
+
 endif
 
 EXCLUDE_GOOGLE_SERVICES_DEVICES += \
