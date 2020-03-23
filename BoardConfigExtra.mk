58d559fcbea1e549346ba04c4861a55ed0392d36
Nolen Johnson <johnsonnolen@gmail.com>
2020-03-23 12:37:31 -0400

    extra: gts4lvwifi: Don't build product image

--- BoardConfigExtra.mk
+++ BoardConfigExtra.mk
@@ -4,7 +4,7 @@ ifneq ($(BOARD_HAS_LARGE_FILESYSTEM),true)
 endif
 
 # Product
-ifneq ($(filter %bonito %sargo %gts4lvwifi %crosshatch %blueline %flame %coral,$(TARGET_PRODUCT)),)
+ifneq ($(filter %bonito %sargo %crosshatch %blueline %flame %coral,$(TARGET_PRODUCT)),)
     TARGET_COPY_OUT_PRODUCT := product
 else
     TARGET_COPY_OUT_PRODUCT := system/product
