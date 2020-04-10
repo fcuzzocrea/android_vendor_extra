c66f1d09d7d800bd5d2d213e1049acb23246151d
Nolen Johnson <johnsonnolen@gmail.com>
2020-04-09 23:15:42 -0400

    extra: Strip down to what 14.1 supports

--- oem-cameras/MotCamera2-denali/MotCamera2-denali.mk
+++
@@ -1,5 +0,0 @@
-PRODUCT_PACKAGES += \
-    MotCamera2-denali
-
-PRODUCT_COPY_FILES += \
-    vendor/extra/oem-cameras/MotCamera2-denali/privapp-permissions-com.motorola.camera2.denali.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-com.motorola.camera2.xml
--- product.mk
+++ product.mk
@@ -6,26 +6,17 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/extra/adb_keys:recovery/root/adb_keys
 
-## Custom Cameras
-# river
-ifneq ($(filter %river,$(TARGET_PRODUCT)),)
--include vendor/extra/oem-cameras/MotCamera2-denali/MotCamera2-denali.mk
-endif
-
 ## Media
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.config.vc_call_vol_steps=7 \
     ro.config.media_vol_steps=25
 
-## OpenWeatherProvider
-PRODUCT_PACKAGES += OpenWeatherProviderPrebuilt
-
 ## Recovery
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += persist.sys.recovery_update=true
 
 ## Updater
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += lineage.updater.uri="https://updater.oddsolutions.us/api/v1/{device}/{type}/{incr}"
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += lineage.updater.allow_downgrading=true
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += cm.updater.uri="https://updater.oddsolutions.us/api/v1/{device}/{type}/{incr}"
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += cm.updater.allow_downgrading=true
 
 ## Signing
 PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/certs/releasekey
