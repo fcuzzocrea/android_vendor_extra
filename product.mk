6ddefba36b7b495f84af88f9875424b5097ac344
Francescodario Cuzzocrea <bosconovic@gmail.com>
2021-10-09 20:54:09 +0200

    extra: adjust for my usecase
    Signed-off-by: Francescodario Cuzzocrea <bosconovic@gmail.com>
    Change-Id: I272a087a821321e1f5272a8a77355a7723ea1b23

--- product.mk
+++ product.mk
@@ -16,10 +16,6 @@ ifneq ($(filter %river,$(TARGET_PRODUCT)),)
 -include vendor/extra/oem-cameras/MotCamera2-denali/MotCamera2-denali.mk
 endif
 
-## fastbootd
-PRODUCT_PACKAGES += fastbootd
-PRODUCT_SYSTEM_PROPERTY_OVERRIDES += ro.fastbootd.available=true
-
 ## ih8sn
 PRODUCT_PACKAGES += ih8sn
 
@@ -31,33 +27,21 @@ PRODUCT_COPY_FILES += \
     vendor/extra/configs/ih8sn/ih8sn_generic.conf:/system/etc/ih8sn.conf
 endif
 
-## Media
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    ro.config.vc_call_vol_steps=7 \
-    ro.config.media_vol_steps=25
 
 ## Overlays
 PRODUCT_PACKAGE_OVERLAYS += vendor/extra/overlay
-
 ## Recovery
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += persist.sys.recovery_update=true
 
 ## Remove Dialer Apps
-ifneq ($(filter %shieldtablet %yellowstone,$(TARGET_PRODUCT)),)
+ifneq ($(filter %shieldtablet %yellowstone %klimtlte,$(TARGET_PRODUCT)),)
 PRODUCT_PACKAGES += remove-dialer
 endif
 
 ## Remove Telephony Apps
-ifneq ($(filter %flo %flox %gts4lvwifi %icosa,$(TARGET_PRODUCT)),)
+ifneq ($(filter %flo %flox %gts4lvwifi %icosa, %klimtlte,$(TARGET_PRODUCT)),)
 PRODUCT_PACKAGES += remove-telephony
 endif
-
-## Updater
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += lineage.updater.uri="https://updater-api.oddsolutions.us/api/v1/{device}/{type}/{incr}"
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += lineage.updater.allow_downgrading=true
-
-## Signing
-PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/certs/releasekey
 
 ifeq ($(WITH_GMS),true)
 ifneq ($(PRODUCT_IS_ATV),true)
