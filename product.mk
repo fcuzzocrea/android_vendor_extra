e6744b52e1179efcb1e7099ec9ee83863e91050f
Francescodario Cuzzocrea <bosconovic@gmail.com>
2021-10-09 20:54:09 +0200

    extra: adjust for my usecase
    Signed-off-by: Francescodario Cuzzocrea <bosconovic@gmail.com>
    Change-Id: I272a087a821321e1f5272a8a77355a7723ea1b23
    Signed-off-by: Francescodario Cuzzocrea <bosconovic@gmail.com>

--- product.mk
+++ product.mk
@@ -1,32 +1,16 @@
 ## ADB Keys
+
 # Android
 PRODUCT_COPY_FILES += \
     vendor/extra/adb_keys:root/adb_keys
+
 # Recovery
 PRODUCT_COPY_FILES += \
     vendor/extra/adb_keys:recovery/root/adb_keys
 
-## ATV
-PRODUCT_PACKAGES += \
-    DocumentsUI
-
 ## Bash
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     persist.sys.adb.shell=/system_ext/bin/bash
-
-## Device-specific
-ifneq ($(filter %TP1803,$(TARGET_PRODUCT)),)
-# TP1803 Camera Additions
-PRODUCT_PACKAGES += \
-    testsig-0x7b401d7c
-endif
-
-## fastbootd
-# Doesn't fit on walleye
-ifeq ($(filter %taimen %walleye,$(TARGET_PRODUCT)),)
-PRODUCT_PACKAGES += fastbootd
-PRODUCT_SYSTEM_PROPERTY_OVERRIDES += ro.fastbootd.available=true
-endif
 
 ## ih8sn
 PRODUCT_PACKAGES += ih8sn
@@ -38,11 +22,6 @@ PRODUCT_PACKAGES += ih8sn
 PRODUCT_COPY_FILES += \
     vendor/extra/configs/ih8sn/ih8sn_generic.conf:/system/etc/ih8sn.conf
 #endif
-
-## Media
-#PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-#    ro.config.vc_call_vol_steps=7 \
-#    ro.config.media_vol_steps=25
 
 ## neofetch
 PRODUCT_COPY_FILES += \
@@ -58,14 +37,6 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += persist.vendor.recovery_update=true
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     setupwizard.feature_deferred_snooze_allow_never=true
 
-## Updater
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += lineage.updater.uri="https://updater-api.oddsolutions.us/api/v1/{device}/{type}/{incr}"
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += lineage.updater.allow_downgrading=true
-
-## Signing
-#PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/lineage-priv/keys/releasekey
-#PRODUCT_OTA_PUBLIC_KEYS := vendor/lineage-priv/keys/otakey.x509.pem
-
 ifeq ($(WITH_GMS),true)
 ifneq ($(PRODUCT_IS_ATV),true)
 -include vendor/google_pixel/product.mk
@@ -77,3 +48,15 @@ PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
     system/bin/ih8sn \
     system/etc/ih8sn.conf \
     system/etc/init/ih8sn.rc
+
+# Enable whole-program R8 Java optimizations for SystemUI and system_server,
+# but also allow explicit overriding for testing and development.
+SYSTEM_OPTIMIZE_JAVA ?= true
+SYSTEMUI_OPTIMIZE_JAVA ?= true
+
+# Preopt SystemUI
+PRODUCT_DEXPREOPT_SPEED_APPS += SystemUI Settings
+
+# Compile SystemUI on device with `speed`.
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.systemuicompilerfilter=speed
