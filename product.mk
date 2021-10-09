f02d0a5af742085227d81572407e2a0d6eb3fcd0
Francescodario Cuzzocrea <bosconovic@gmail.com>
2021-10-09 20:54:09 +0200

    extra: adjust for my usecase
    Change-Id: I272a087a821321e1f5272a8a77355a7723ea1b23
    Signed-off-by: Francescodario Cuzzocrea <bosconovic@gmail.com>

--- product.mk
+++ product.mk
@@ -1,66 +1,31 @@
 ## ADB Keys
+
 # Android
 PRODUCT_COPY_FILES += \
     vendor/extra/adb_keys:root/adb_keys
+
 # Recovery
 PRODUCT_COPY_FILES += \
     vendor/extra/adb_keys:recovery/root/adb_keys
-
-## ATV
-PRODUCT_PACKAGES += \
-    DocumentsUI
 
 ## Bash
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     persist.sys.adb.shell=/system_ext/bin/bash
 
 ## Debugging
-ifeq ($(filter %foster %foster_tab %sif %sphynx %nx %nx_tab,$(TARGET_PRODUCT)),)
+ifeq ($(filter %foster %foster_tab %porg %porg_tab %sif %sphynx %nx %nx_tab,$(TARGET_PRODUCT)),)
 ifneq ($(TARGET_BUILD_VARIANT),eng)
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += log.tag=I
 endif
 endif
 
-## Device-specific
-ifneq ($(filter %TP1803,$(TARGET_PRODUCT)),)
-# TP1803 Camera Additions
-PRODUCT_PACKAGES += \
-    testsig-0x7b401d7c
-endif
-# sif ADB over Wi-Fi/Ethernet
-ifneq ($(filter %baracus %baracus_tab %sif,$(TARGET_PRODUCT)),)
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += service.adb.tcp.port=5555
-endif
-# radxa02 USB keyboard instead of remote
-ifneq ($(filter %radxa02,$(TARGET_PRODUCT)),)
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += atv.setup.bt_remote_pairing=false
-endif
-
-## fastbootd
-# Doesn't fit on walleye
-ifeq ($(filter %taimen %walleye,$(TARGET_PRODUCT)),)
-PRODUCT_PACKAGES += fastbootd
-PRODUCT_SYSTEM_PROPERTY_OVERRIDES += ro.fastbootd.available=true
-endif
-
 ## ih8sn
 PRODUCT_PACKAGES += ih8sn
 
-#ifneq ("$(wildcard  vendor/extra/configs/ih8sn/ih8sn_$(subst lineage_,,$(TARGET_PRODUCT)).conf)","")
-#PRODUCT_COPY_FILES += \
-#    vendor/extra/configs/ih8sn/ih8sn_$(subst lineage_,,$(TARGET_PRODUCT)).conf:/system/etc/ih8sn.conf
-#else
 PRODUCT_COPY_FILES += \
     vendor/extra/configs/ih8sn/ih8sn_generic.conf:/system/etc/ih8sn.conf
-#endif
-
 ## Logging
 $(call soong_config_set,liblog,force_debuggable,true)
-
-## Media
-#PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-#    ro.config.vc_call_vol_steps=7 \
-#    ro.config.media_vol_steps=25
 
 ## neofetch
 PRODUCT_COPY_FILES += \
@@ -77,19 +42,10 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += persist.vendor.recovery_allow_spl_downgrade
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     setupwizard.feature_deferred_snooze_allow_never=true
 
-## Updater
-PRODUCT_SYSTEM_EXT_PROPERTIES += lineage.updater.uri="https://updater-api.oddsolutions.us/api/v1/{device}/{type}/{incr}"
-PRODUCT_SYSTEM_EXT_PROPERTIES += lineage.updater.allow_downgrading=true
-PRODUCT_SYSTEM_EXT_PROPERTIES += lineage.updater.allow_major_upgrades
-
-## Signing
-#PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/lineage-priv/keys/releasekey
-#PRODUCT_OTA_PUBLIC_KEYS := vendor/lineage-priv/keys/otakey.x509.pem
-
 ifeq ($(WITH_GMS),true)
 ifneq ($(PRODUCT_IS_ATV),true)
 ifneq ($(PRODUCT_IS_AUTOMOTIVE),true)
--include vendor/google_pixel/product.mk
+$(call inherit-product, vendor/google_pixel/products/google_pixel.mk)
 endif # PRODUCT_IS_AUTOMOTIVE
 endif # PRODUCT_IS_ATV
 endif # WITH_GMS
@@ -99,3 +55,16 @@ PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
     system/bin/ih8sn \
     system/etc/ih8sn.conf \
     system/etc/init/ih8sn.rc
+
+# Enable whole-program R8 Java optimizations for SystemUI and system_server,
+# but also allow explicit overriding for testing and development.
+SYSTEM_OPTIMIZE_JAVA ?= true
+SYSTEMUI_OPTIMIZE_JAVA ?= true
+
+# Preopt SystemUI
+PRODUCT_DEXPREOPT_SPEED_APPS += SystemUI Settings
+
+# Compile SystemUI on device with `speed`.
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.systemuicompilerfilter=speed
+
