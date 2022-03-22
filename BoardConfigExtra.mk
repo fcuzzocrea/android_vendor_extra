3946d01221fc32f1d9bb89f5b01b1e317d926d6c
Jarl-Penguin <jarlpenguin@outlook.com>
2022-03-22 17:09:29 -0400

    extra: Opt-in to shipping full recovery image by default
    * When the boot image is modified, recovery updating fails
      because the patch no longer applies to the modified boot
      image, so ship a full recovery image instead of a patch
      so recovery updating always works.
    
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>
    Change-Id: I56cf6c4d301afa88b39b31d488c5166903406637

--- BoardConfigExtra.mk
+++ BoardConfigExtra.mk
@@ -3,6 +3,9 @@ ifndef TARGET_COPY_OUT_PRODUCT
     TARGET_COPY_OUT_PRODUCT := system/product
 endif
 
+# Recovery
+BOARD_USES_FULL_RECOVERY_IMAGE ?= true
+
 # System_ext
 ifndef TARGET_COPY_OUT_SYSTEM_EXT
     TARGET_COPY_OUT_SYSTEM_EXT := system/system_ext
